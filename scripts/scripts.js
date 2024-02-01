f10cf4198760a75ac9e0e4baa1c9a0d0ef856a6a
sai-2409 <sadriddin.2409@gmail.com>
2024-02-01 12:48:40 -0800

    finish making nav for site

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,19 @@
 document.getElementById("main__button-id").onclick = function () {
    document.getElementById("main__footer-id").scrollIntoView({ behavior: "smooth" });
 }
+
+document.getElementById("big__main-button").onclick = function () {
+   document.getElementById("main__footer-id").scrollIntoView({ behavior: "smooth" });
+}
+document.getElementById("learn__button-id").onclick = function () {
+   document.getElementById("main__footer-id").scrollIntoView({ behavior: "smooth" });
+}
+document.getElementById("learn__button-id2").onclick = function () {
+   document.getElementById("main__footer-id").scrollIntoView({ behavior: "smooth" });
+}
+document.getElementById("learn__button-id3").onclick = function () {
+   document.getElementById("main__footer-id").scrollIntoView({ behavior: "smooth" });
+}
+document.getElementById("learn__button-id4").onclick = function () {
+   document.getElementById("main__footer-id").scrollIntoView({ behavior: "smooth" });
+}
